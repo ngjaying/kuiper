ca8b072f99112482dad1d62156d7997c36903e86
ngjaying <ngjaying@gmail.com>
2020-03-13 12:32:43 +0800

    feat(memory): pprof for memory

--- xstream/server/main.go
+++ xstream/server/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "github.com/emqx/kuiper/xstream/server/server"
+import (
+	"github.com/emqx/kuiper/xstream/server/server"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+)
 
 var Version string = "unknown"
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.SetOutput(os.Stdout)
+
+	runtime.GOMAXPROCS(1)
+	runtime.SetMutexProfileFraction(1)
+	runtime.SetBlockProfileRate(1)
+
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+
 	server.StartUp(Version)
 }
