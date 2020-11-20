f9b428f5dd17830504e6bf89ed9d28fc5c5f9b6f
ngjaying <ngjaying@gmail.com>
2020-11-20 11:02:19 +0800

    add pprof

--- xstream/server/main.go
+++ xstream/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/emqx/kuiper/xstream/server/server"
+import (
+	"github.com/emqx/kuiper/xstream/server/server"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
 
 var (
 	Version      = "unknown"
@@ -8,5 +13,8 @@ var (
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	server.StartUp(Version, LoadFileType)
 }
